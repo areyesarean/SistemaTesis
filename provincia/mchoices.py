7b41751beb8d4acda107b500603d9065da9b4d15
areyesarean <alereyarean@gmail.com>
2021-09-30 08:36:14 +0200

    creado modelo provincia

---
+++ provincia/mchoices.py
@@ -0,0 +1,21 @@
+sexo = (
+    ('Masculino', 'Masculino'),
+    ('Femenino', 'Femenino'),
+    ('Gary', 'Gary'),
+)
+Provincias = (
+    ('ISL', 'Isla de la juventud'),
+    ('PRI', 'Pinar del río'),
+    ('LHA', 'La Habana'),
+    ('MAY', 'Mayabeque'),
+    ('ART', 'Artemisa'),
+    ('MAT', 'Matanzas'),
+    ('CAV', 'Ciego de Ávila'),
+    ('CFG', 'Cienfuegos'),
+    ('CAM', 'Camaguey'),
+    ('Las tunas', 'Las tunas'),
+    ('Granma', 'Granma'),
+    ('HOL', 'Holguín'),
+    ('SCU', 'Santiago de Cuba'),
+    ('GTM', 'Guantánamo'),
+)
