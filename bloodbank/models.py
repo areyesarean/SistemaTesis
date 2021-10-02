c3a1676d122b15bf23e2b5c7ae4788b407f8b5a3
areyesarean <alereyarean@gmail.com>
2021-10-02 08:27:17 +0200

    create model bloodBank

--- bloodbank/models.py
+++ bloodbank/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
 # Create your models here.
+from django.forms import model_to_dict
+
+from municipio.models import Municipios
+
+
+class BloodBank(models.Model):
+    nombre = models.CharField('Nombre', unique=True, max_length=60)
+    municipio = models.ForeignKey(Municipios, on_delete=models.PROTECT, verbose_name='Municipio')
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'BloodBank'
+        verbose_name_plural = 'BloodBanks'
+        # ordering = ['municipio']
+
+    def toJson(self):
+        item = model_to_dict(self)
+        item['municipio'] = self.municipio.municipio
+        return item
