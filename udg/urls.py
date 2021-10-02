a76806c07eb592ee538d9fdeab78f99c0d979c81
areyesarean <alereyarean@gmail.com>
2021-10-02 08:18:39 +0200

    register url in url projects

--- udg/urls.py
+++ udg/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('provincia/', include('provincia.urls')),
     path('municipio/', include('municipio.urls')),
     path('areasalud/', include('areasalud.urls')),
+    path('bloodbank/', include('bloodbank.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
