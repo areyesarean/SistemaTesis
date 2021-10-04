10f8f8b3c1816a93e3fd1d53e1f59f14128d5218
areyesarean <alereyarean@gmail.com>
2021-10-04 10:17:14 +0200

    create model bloodgroup

--- bloodgroup/models.py
+++ bloodgroup/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.forms import model_to_dict
+
+
+class BloodGroup(models.Model):
+    bloodgroup = models.CharField('Grupo Sanguineo', unique=True, max_length=5)
+
+    def __str__(self):
+        return '{}'.format(self.bloodgroup)
+
+    class Meta:
+        verbose_name = 'BloodGroup'
+        verbose_name_plural = 'BloodGroups'
+        # ordering = ['municipio']
+
+    def toJson(self):
+        item = model_to_dict(self)
+        return item
