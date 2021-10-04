f1554e685862495daced1f5ec17d642b47de95e7
areyesarean <alereyarean@gmail.com>
2021-10-04 14:21:36 +0200

    create validator of ci an age in model donante

--- core/validators.py
+++ core/validators.py
@@ -4,6 +4,6 @@ CI_LENGTH_VALIDATORS = MinLengthValidator(11, message='El carné debe contener 1
 CI_ONLY_DIGITS_VALIDATOR = RegexValidator('^[0-9]{11}$', message='Carné incorrecto')
 CI_VALIDATORS = [CI_ONLY_DIGITS_VALIDATOR, CI_LENGTH_VALIDATORS]
 
-EDAD_MIN_VALUE_VALIDATOR = MinValueValidator(5, message='La edad no puede ser menor a 5 años')
-EDAD_MAX_VALUE_VALIDATOR = MaxValueValidator(25, message='La edad no puede ser superior a 25 años')
+EDAD_MIN_VALUE_VALIDATOR = MinValueValidator(18, message='La edad no puede ser menor a 18 años')
+EDAD_MAX_VALUE_VALIDATOR = MaxValueValidator(80, message='La edad no puede ser superior a 80 años')
 EDAD_VALIDATORS = [EDAD_MAX_VALUE_VALIDATOR, EDAD_MIN_VALUE_VALIDATOR]
