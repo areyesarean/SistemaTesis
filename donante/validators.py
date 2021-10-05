3cedc27f558ed4fbcd86c771afd82fab662db301
areyesarean <alereyarean@gmail.com>
2021-10-05 15:59:10 +0200

    create donante validators

--- donante/validators.py
+++ donante/validators.py
@@ -5,5 +5,5 @@ CI_ONLY_DIGITS_VALIDATOR = RegexValidator('^[0-9]{11}$', message='Carné incorre
 CI_VALIDATORS = [CI_ONLY_DIGITS_VALIDATOR, CI_LENGTH_VALIDATORS]
 
 EDAD_MIN_VALUE_VALIDATOR = MinValueValidator(5, message='La edad no puede ser menor a 5 años')
-EDAD_MAX_VALUE_VALIDATOR = MaxValueValidator(25, message='La edad no puede ser superior a 25 años')
+EDAD_MAX_VALUE_VALIDATOR = MaxValueValidator(70, message='La edad no puede ser superior a 70 años')
 EDAD_VALIDATORS = [EDAD_MAX_VALUE_VALIDATOR, EDAD_MIN_VALUE_VALIDATOR]
