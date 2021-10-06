d1415fc1d0fd328a7b5b4d344ce05e1fcafef32c
areyesarean <alereyarean@gmail.com>
2021-10-06 17:32:12 +0200

    graficos de pantalla principal

--- interfaz/views.py
+++ interfaz/views.py
@@ -3,7 +3,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 from django.views.generic import TemplateView
 
+from areasalud.models import AreaSalud
+from bloodbank.models import BloodBank
+from consultorio.models import Consultorio
 from core.models import Estudiante, Sexo, Municipio
+from donante.models import Donante
+from sexo.models import Sexos
 
 
 class MenuPpal(LoginRequiredMixin, TemplateView):
@@ -11,23 +16,29 @@ class MenuPpal(LoginRequiredMixin, TemplateView):
 
     def graphic_sexo(self):
         data = []
-        for i in Sexo.objects.all():
+        for i in Sexos.objects.all():
             data.append({
                 'name': i.sexo,
-                'y': Estudiante.objects.filter(sexo__sexo=i).count()
+                'y': Donante.objects.filter(sexo__sexo=i).count()
             })
         return data
 
     def graphic_catMunicipio(self):
         data = []
-        for i in Municipio.objects.all():
-            data.append(Estudiante.objects.filter(municipio__municipio=i).count())
+        for i in AreaSalud.objects.all():
+            data.append(Donante.objects.filter(consultorio__areasalud=i).count())
         return data
 
     def get_municipios(self):
         data = []
         for i in Municipio.objects.all():
             data.append(i.municipio)
+        return data
+
+    def get_areasalud(self):
+        data = []
+        for i in AreaSalud.objects.all():
+            data.append(i.nombre)
         return data
 
     def cant_estudiantes(self):
@@ -38,6 +49,11 @@ class MenuPpal(LoginRequiredMixin, TemplateView):
         context['title'] = 'Página Principal'
         context['data'] = self.graphic_sexo()
         context['municipios'] = self.get_municipios()
+        context['areasalud'] = self.get_areasalud()
+        context['cant_areasalud'] = AreaSalud.objects.count()
+        context['cant_blood_bank'] = BloodBank.objects.count()
+        context['cant_donantes'] = Donante.objects.count()
+        context['cant_consultorios'] = Consultorio.objects.count()
         context['data_municipios'] = self.graphic_catMunicipio()
         context['cant_estudiantes'] = self.cant_estudiantes()
         return context
