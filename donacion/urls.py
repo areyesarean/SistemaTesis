0cf2ef5d4616288fa657991da56dc433bc1ba9c1
areyesarean <alereyarean@gmail.com>
2021-10-11 20:14:19 +0200

    create url donacion

--- donacion/urls.py
+++ donacion/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 
-from consultorio.views import ListConsultorio, UpdateConsultorio
-from donacion.views import AddDonacion, AddDonacionP
+from donacion.views import AddDonacion, ListDonacion, UpdateDonacion
 
 app_name = 'donacion'
 
 urlpatterns = [
-    path('list/', ListConsultorio.as_view(), name='ListDonacion'),
+    path('list/', ListDonacion.as_view(), name='ListDonacion'),
     path('add/', AddDonacion.as_view(), name='AddDonacion'),
-    path('adds/', AddDonacionP.as_view(), name='AddDonacionP'),
-    path('update/<int:pk>', UpdateConsultorio.as_view(), name='UpdateDonacion'),
+    path('update/<int:pk>', UpdateDonacion.as_view(), name='UpdateDonacion'),
     # path('delete/<int:pk>', DeleteEstudiante.as_view(), name='DeleteEstudiante'),
     # path('pdf/', PdfEstudiantesView.as_view(), name='PdfEstudiantesView'),
 ]
