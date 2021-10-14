d5a1cf2ec01deb81a456e9572316eead586ecf96
areyesarean <alereyarean@gmail.com>
2021-10-14 23:42:25 +0200

    add bloodgroup_choices in row bloodgroup in the model

--- bloodgroup/models.py
+++ bloodgroup/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
 # Create your models here.
 from django.forms import model_to_dict
 
+from bloodgroup.bloodgroup_choices import bloodgroup
+
 
 class BloodGroup(models.Model):
-    bloodgroup = models.CharField('Grupo Sanguineo', unique=True, max_length=5)
+    bloodgroup = models.CharField('Grupo Sanguineo', choices=bloodgroup, unique=True, max_length=5)
 
     def __str__(self):
         return '{}'.format(self.bloodgroup)
