fb6a173a14954f565b36a458ed69799058fd9e00
areyesarean <alereyarean@gmail.com>
2021-10-19 19:49:10 +0200

    add model the appCore to the admin site

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-# from core.models import Sexo, Estudiante, Provincia, Municipio
-#
-#
-# class EstudianteAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'ci', 'nombre', 'age', 'sexo', 'provincia', 'municipio', ]
-#     search_fields = ['ci', 'nombre', 'age', 'municipio__municipio', 'sexo__sexo', 'provincia__provincia']
-#     list_filter = ['age', 'sexo', 'municipio']
-#
-#
-# admin.site.register(Sexo)
-# admin.site.register(Estudiante, EstudianteAdmin)
-# admin.site.register(Provincia)
-# admin.site.register(Municipio)
+from core.models import Sexo, Estudiante, Provincia, Municipio
+
+
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'ci', 'nombre', 'age', 'sexo', 'provincia', 'municipio', ]
+    search_fields = ['ci', 'nombre', 'age', 'municipio__municipio', 'sexo__sexo', 'provincia__provincia']
+    list_filter = ['age', 'sexo', 'municipio']
+
+
+admin.site.register(Sexo)
+admin.site.register(Estudiante, EstudianteAdmin)
+admin.site.register(Provincia)
+admin.site.register(Municipio)
