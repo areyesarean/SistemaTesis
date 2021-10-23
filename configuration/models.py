064291e3a34177e10c6949ec1d92b4dcaa578bc0
areyesarean <alereyarean@gmail.com>
2021-10-23 16:34:41 +0200

    create model Configuration

--- configuration/models.py
+++ configuration/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from django.forms import model_to_dict
+
+
+class Configuration(models.Model):
+    don_mensu = models.IntegerField(verbose_name='Cantidad de Donaciones Mensuales', default=3)
+
+    def __str__(self):
+        return '{}'.format(self.don_mensu)
+
+    class Meta:
+        verbose_name = 'Configuration'
+        verbose_name_plural = 'Configurations'
+        # ordering = ['municipio']
+
+    def toJson(self):
+        item = model_to_dict(self)
+        return item
